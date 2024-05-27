507c3e5948c10bd6c82d43f066f61fd686066497
José Quiñónez <joserobertoq@outlook.com>
2024-05-27 15:48:04 -0600

    Add Pending Tasks

---
+++ Tasker/MVVM/Models/MyTask.cs
@@ -0,0 +1,12 @@
+﻿namespace Tasker.MVVM.Models;
+
+public class MyTask
+{
+    public required string TaskName { get; set; }
+
+    public bool Completed { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string TaskColor { get; set; }
+}
