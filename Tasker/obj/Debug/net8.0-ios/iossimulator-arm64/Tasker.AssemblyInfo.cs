aa7bb34c379a02cfcf0a5f2fe861593adb7892cb
José Quiñónez <joserobertoq@outlook.com>
2024-05-27 16:02:17 -0600

    Add Round Rectangle Style

--- Tasker/obj/Debug/net8.0-ios/iossimulator-arm64/Tasker.AssemblyInfo.cs
+++ Tasker/obj/Debug/net8.0-ios/iossimulator-arm64/Tasker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6f5f6bc2244e3852688e060b638eae108cd01521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+507c3e5948c10bd6c82d43f066f61fd686066497")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
